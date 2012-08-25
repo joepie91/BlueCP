2802c59bb7992475bd22371d65cd4dcb59de9a58
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-08-26 01:02:45 +0200

    Add check for empty id variable

--- index.php
+++ index.php
@@ -4,13 +4,16 @@ include('./includes/loader.php');
 	if($LoggedIn === true){
 		header("Location: member_home.php");
 	} else {
-
-		if($_GET['id'] == 'login'){
-		$error_message = User::login($_POST['username'], $_POST['password']); /* I wasn't using GET for this I just made a mistake. The HTML was POST... */
+		if(!empty($_GET['id']))
+		{
+			if($_GET['id'] == 'login'){
+				$error_message = User::login($_POST['username'], $_POST['password']); /* I wasn't using GET for this I just made a mistake. The HTML was POST... */
+			}
 		}
+			
 		echo Templater::AdvancedParse('/blue_default/index', $locale->strings, array(
-		'LoggedIn' => $LoggedIn,
-		'PageTitle'  => 'BlueCP Login'
+			'LoggedIn' => $LoggedIn,
+			'PageTitle'  => 'BlueCP Login'
 		));
 	}
 ?>
